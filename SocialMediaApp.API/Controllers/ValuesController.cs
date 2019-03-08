57eb5bbd28eb15fdd38ef6b39a52f4b903d78b40
Jake Ford <jake.ford@hexagonsi.com>
2019-03-08 14:14:03 -0600

    Initial Commit

---
+++ SocialMediaApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMediaApp.API.Data;
+
+namespace SocialMediaApp.API.Controllers
+{
+    // http://localhost:5000/api/values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+
+        }
+        // GET api/values
+        // [HttpGet] OLD SYNCHRONOUS METHOD
+        // public IActionResult GetValues() //IActionResult allows you to return HTTP Responses to the client
+        // {
+        //     var values = context.Values.ToList(); //This gives values to EF (Entity Framework) methods, as well as DB Sets (Values)
+        //     return Ok(values);
+        // }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues() //IActionResult allows you to return HTTP Responses to the client
+        {
+            var values = await context.Values.ToListAsync(); //This gives values to EF (Entity Framework) methods, as well as DB Sets (Values)
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            //FirstOrDefault returns null if no matching value is found, while First() just returns an exception.
+            //The 1st "x" in the Lambda expression represents the value we are returning.
+            //x.Id is going to match the value that is being passed in(x.Id == id)
+            var value = await context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SocialMediaApp.API/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SocialMediaApp.API.Data;
+
+namespace SocialMediaApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors(); // Cross Origin Resource Sharing, CORS  - Security measure that controls which clients(Domains) can access our api
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts();
+            }
+
+            // app.UseHttpsRedirection();
+
+            // The below CORS configuration is weak, security wise, allowing any HTTP origin, method, or header;
+            // but it will work for development instead of adding a secure CORS policy, which would take more time.
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); //Allows us to talk to our angular app (localhost:4200)
+            app.UseMvc(); // Must initialize CORS or anything else before Mvc usually.
+        }
+    }
+}
